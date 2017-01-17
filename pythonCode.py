555ae650cef6b1bfd4fe2cf6ae8578967c3ecde1
grubdragon <grubdragon@users.noreply.github.com>
2017-01-17 19:42:58 +0530

    Create pythonCode.py

---
+++ pythonCode.py
@@ -0,0 +1,27 @@
+import wave
+import time
+import socket
+ip = '192.168.43.184'
+waveFile = wave.open('bird.wav', 'r')
+t = 1./waveFile.getframerate()
+length = waveFile.getnframes()
+sampwid = waveFile.getsampwidth()
+
+print(t,1/t,length,sampwid)
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((ip,80))
+
+def send(data):
+    n = s.send(data)
+    if n==0:
+        print("Sorry rahega")
+        exit()
+
+for i in xrange(0,length):
+    waveData = waveFile.readframes(1)[0]
+    send(waveData)
+    #print("Sent: %d" % ord(waveData))
+    time.sleep(t-0.000002)
+
+s.close()
