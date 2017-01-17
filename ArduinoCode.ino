462b7f1dea4568490bcf82d6a25fe3d186e0e511
grubdragon <grubdragon@users.noreply.github.com>
2017-01-17 19:42:33 +0530

    Create ArduinoCode.ino

---
+++ ArduinoCode.ino
@@ -0,0 +1,78 @@
+/*
+ *  This sketch demonstrates how to set up a simple HTTP-like server.
+ *  The server will set a GPIO pin depending on the request
+ *    http://server_ip/gpio/0 will set the GPIO2 low,
+ *    http://server_ip/gpio/1 will set the GPIO2 high
+ *  server_ip is the IP address of the ESP8266 module, will be 
+ *  printed to Serial when the module is connected.
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Akshay";
+const char* password = "12345678";
+unsigned long after,before;
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // prepare GPIO2
+  pinMode(2, OUTPUT);
+  digitalWrite(2, 0);
+  
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  char req;
+  // Read the first char of the request
+  before = micros();
+  while(true)
+  {
+    req = client.read();
+    if(req==255){
+      continue;
+    }
+    Serial.println((int)req);
+    analogWrite(2,(((int)req)+128)*4);
+    after=micros();
+    Serial.println(after-before);
+    before=after;
+  }
+  Serial.println("Done!");
+ }
